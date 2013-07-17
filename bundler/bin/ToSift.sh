ee20cc1db034a8f0c7cbb0cc5411317c945fc987
ibraheem <ennetws@gmail.com>
2013-07-17 01:59:33 +0000

    Safe script

--- bundler/bin/ToSift.sh
+++ bundler/bin/ToSift.sh
@@ -33,7 +33,7 @@ for d in `ls -1 $IMAGE_DIR | egrep "jpg$"`
 do 
     pgm_file=$IMAGE_DIR/`echo $d | sed 's/jpg$/pgm/'`
     key_file=$IMAGE_DIR/`echo $d | sed 's/jpg$/key/'`
-    (mogrify -format pgm $IMAGE_DIR/$d; $SIFT < $pgm_file > $key_file; rm $pgm_file; gzip -f $key_file) &
+    mogrify -format pgm $IMAGE_DIR/$d; $SIFT < $pgm_file > $key_file; rm $pgm_file; gzip -f $key_file
 done
 
 wait
