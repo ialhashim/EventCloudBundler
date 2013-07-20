2fe09f1e98afa45c9208981d29fd50895a9b8e50
root <root@localhost.localdomain>
2013-07-19 20:19:37 -0700

    Everything works

--- preprocessMedia.sh
+++ preprocessMedia.sh
@@ -2,14 +2,21 @@
 files=$(find data/$1/full/*.jpg)
 
 # Resolution
-r=${2:-500}
+r=${2:-0}
 
 # Output folder
 mkdir -p output/$1
 
 for file in $files; do
 	filename=${file##*/}
-	cmmand="convert $file -resize "${r}x${r}^" output/$1/$filename"
+
+	if [ $r -eq 0 ] 
+	then
+		cmmand="convert $file output/$1/$filename"
+	else
+		cmmand="convert $file -resize "${r}x${r}^" output/$1/$filename"
+	fi	
+
 	echo $cmmand
 	$cmmand
 done
